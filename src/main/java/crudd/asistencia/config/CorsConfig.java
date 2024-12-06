26bafcafc3f793c42837ec8f6d5728bace957598
AngelaFernandez02 <Angieglez20f@Outlook.com>
2024-12-05 19:17:29 -0600

    CAMBIOS CORSS

--- src/main/java/crudd/asistencia/config/CorsConfig.java
+++ src/main/java/crudd/asistencia/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica a todas las rutas
-                        .allowedOrigins("/**") // Dirección del frontend
+                        .allowedOrigins("http://127.0.0.1:5500") // Dirección del frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // Permite el uso de cookies o autenticación
--- src/main/java/crudd/asistencia/controller/ControlAsistenciaController.java
+++ src/main/java/crudd/asistencia/controller/ControlAsistenciaController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/controlAsistencia")
+@CrossOrigin(origins = "/**")
 public class ControlAsistenciaController {
 
 
