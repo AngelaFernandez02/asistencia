9f630ca2fa9eacd9abbaeb03dd42e35b6a367665
AngelaFernandez02 <Angieglez20f@Outlook.com>
2024-12-05 19:55:27 -0600

    SOLUCION

--- src/main/java/crudd/asistencia/config/CorsConfig.java
+++ src/main/java/crudd/asistencia/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica a todas las rutas
-                        .allowedOrigins("http://127.0.0.1:5500") // Dirección del frontend
+                        .allowedOrigins("/**") // Dirección del frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // Permite el uso de cookies o autenticación
